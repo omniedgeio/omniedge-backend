e802e8bc5508a022fc55bcc1d6d9538c0355943b
Tex Tang <tex_tang@outlook.com>
2021-12-16 17:56:12 +0800

    Added routes & controllers

--- start/routes.ts
+++ start/routes.ts
@@ -18,8 +18,65 @@
 |
 */
 
-import Route from '@ioc:Adonis/Core/Route'
+import Route from "@ioc:Adonis/Core/Route";
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.group(() => {
+  Route.group(() => {
+    Route.post("/register", "AuthController.register");
+    Route.post("/login/google", "AuthController.loginWithGoogle");
+    Route.post("/login/password", "AuthController.loginWithPassword");
+    Route.post("/login/security-key", "AuthController.loginWithSecurityKey");
+    Route.post("/reset-password/code", "AuthController.resetPasswordWithCode");
+    Route.post(
+      "/reset-password/verify",
+      "AuthController.resetPasswordWithVerification"
+    );
+
+    // Features
+    Route.post("/verify-email", "AuthController.verifyEmail");
+  }).prefix("/auth");
+
+  Route.group(() => {
+    Route.get("/", "ProfileController.index");
+    Route.put("/", "ProfileController.update");
+    Route.put("/change-password", "ProfileController.changePassword");
+  }).prefix("/profile");
+
+  Route.group(() => {
+    Route.post("/", "VirtualNetworksController.create");
+    Route.get("/", "VirtualNetworksController.list");
+    Route.get("/:id", "VirtualNetworksController.retrieve");
+    Route.put("/:id", "VirtualNetworksController.update");
+    Route.delete("/:id", "VirtualNetworksController.delete");
+  }).prefix("/virtual-networks");
+
+  Route.group(() => {
+    Route.post("/", "DevicesController.create");
+    Route.get("/", "DevicesController.list");
+    Route.get("/:id", "DevicesController.retrieve");
+    Route.put("/:id", "DevicesController.update");
+    Route.delete("/:id", "DevicesController.delete");
+  }).prefix("/devices");
+
+  Route.group(() => {
+    Route.post("/", "InvitationController.create");
+    Route.get("/", "InvitationController.list");
+    Route.put("/:id", "InvitationController.update");
+    Route.delete("/:id", "InvitationController.delete");
+  }).prefix("/invitations");
+
+  Route.group(() => {
+    Route.post("/", "UsersController.create");
+    Route.get("/", "UsersController.list");
+    Route.get("/:id", "UsersController.retrieve");
+    Route.put("/:id", "UsersController.update");
+    Route.delete("/:id", "UsersController.delete");
+  }).prefix("/users");
+
+  Route.group(() => {
+    Route.post("/", "SecurityKeysController.create");
+    Route.get("/", "SecurityKeysController.list");
+    Route.put("/:id", "SecurityKeysController.update");
+    Route.delete("/:id", "SecurityKeysController.delete");
+  }).prefix("/security-keys");
+}).prefix("/api");
