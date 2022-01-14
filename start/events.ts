ca9fe173d419177f5949ec077d06a13e3cca5cb5
Tex Tang <tex_tang@outlook.com>
2022-01-14 22:43:42 +0800

    fix: hide sql event

--- start/events.ts
+++ start/events.ts
@@ -1,21 +1,21 @@
-import Application from '@ioc:Adonis/Core/Application'
-/*
-|--------------------------------------------------------------------------
-| Preloaded File
-|--------------------------------------------------------------------------
-|
-| Any code written inside this file will be executed during the application
-| boot.
-|
-*/
-import Event from '@ioc:Adonis/Core/Event'
-import Logger from '@ioc:Adonis/Core/Logger'
-import Database from '@ioc:Adonis/Lucid/Database'
+// import Application from '@ioc:Adonis/Core/Application'
+// /*
+// |--------------------------------------------------------------------------
+// | Preloaded File
+// |--------------------------------------------------------------------------
+// |
+// | Any code written inside this file will be executed during the application
+// | boot.
+// |
+// */
+// import Event from '@ioc:Adonis/Core/Event'
+// import Logger from '@ioc:Adonis/Core/Logger'
+// import Database from '@ioc:Adonis/Lucid/Database'
 
-Event.on('db:query', (query) => {
-  if (Application.inProduction) {
-    Logger.debug('sql: %s %o', query.sql, query.bindings)
-  } else {
-    Database.prettyPrint(query)
-  }
-})
+// // Event.on('db:query', (query) => {
+// //   if (Application.inProduction) {
+// //     Logger.debug('sql: %s %o', query.sql, query.bindings)
+// //   } else {
+// //     Database.prettyPrint(query)
+// //   }
+// // })
