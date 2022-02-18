6ca94a6084d5004e2638b8d0857a1a06dbaa66e2
Tex Tang <tex_tang@outlook.com>
2022-02-18 12:05:39 +0800

    refactor(): add tests

---
+++ japaFile.ts
@@ -0,0 +1,23 @@
+import getPort from 'get-port'
+import { configure } from 'japa'
+import { join } from 'path'
+import 'reflect-metadata'
+import sourceMapSupport from 'source-map-support'
+
+process.env.NODE_ENV = 'testing'
+process.env.ADONIS_ACE_CWD = join(__dirname)
+sourceMapSupport.install({ handleUncaughtExceptions: false })
+
+async function startHttpServer() {
+  const { Ignitor } = await import('@adonisjs/core/build/src/Ignitor')
+  process.env.PORT = String(await getPort())
+  await new Ignitor(__dirname).httpServer().start()
+}
+
+/**
+ * Configure test runner
+ */
+configure({
+  files: ['test/**/*.spec.ts'],
+  before: [startHttpServer],
+})
---
+++ test/utils.spec.ts
@@ -0,0 +1,36 @@
+import test from 'japa'
+import { ip2long, long2ip, Netmask } from 'netmask'
+import { getAvailableIPRange, nextUnassignedIP } from '../utils/ip'
+
+test.group('utils.ip', () => {
+  test('available ip range', (assert) => {
+    const block = new Netmask('100.100.0.0/24')
+    const usedIPs = ['100.100.0.1', '100.100.0.100']
+    const ipRange = getAvailableIPRange(
+      ip2long(block.first),
+      ip2long(block.last),
+      usedIPs.map((v) => ip2long(v))
+    )
+
+    assert.equal(ipRange.length, 2)
+    assert.equal(long2ip(ipRange[0][0]), '100.100.0.2')
+    assert.equal(long2ip(ipRange[ipRange.length - 1][0]), '100.100.0.101')
+  })
+
+  test('random ip from 256 addresses', (assert) => {
+    const usedIPs = ['100.100.0.1', '100.100.0.100']
+    const cidr = '100.100.0.0/24'
+    const block = new Netmask(cidr)
+
+    const availableIPLength = block.size - usedIPs.length - 2
+    for (let i = 0; i < availableIPLength; i++) {
+      const nextIP = nextUnassignedIP(cidr, usedIPs)
+      assert.isNotNull(nextIP)
+      if (nextIP) {
+        assert.isTrue(!usedIPs.includes(nextIP), `IP '${nextIP}' is already used`)
+        usedIPs.push(nextIP)
+      }
+    }
+    assert.equal(usedIPs.length, block.size - 2)
+  })
+})
